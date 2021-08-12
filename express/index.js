d7ff8527a01fd021899e9aa148126c40963b78f2
Bogdan Silvasan <bogdan.silve@gmail.com>
2021-08-12 15:18:34 +0300

    Added cors

--- express/index.js
+++ express/index.js
@@ -1,10 +1,14 @@
 const app = require('express')();
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const PORT = 8080;
 
+app.use(cors());
+
 app.get('/', (req, res) => {
-    let file = fs.readFileSync('./microbit_v2.bin', 'binary');
+    let file = fs.readFileSync('./microbit_v2.bin');
+    console.log(file);
     res.status(200).json({file: file});
 });
 
