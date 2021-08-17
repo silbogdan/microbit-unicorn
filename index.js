6a34c9503e0b3ab3af86beb632ca1f6bf8314d48
Serban Andrei <usadekall@gmail.com>
2021-08-17 14:53:45 +0300

    xD

--- index.js
+++ index.js
@@ -29,12 +29,14 @@ function pcWrite(value) {
 }
 
 function read_fn (handle, type, addr_lo, addr_hi, size, value_lo, value_hi, user_data) {
-    console.log (`mem read 0x${addr_lo.toString(16)}`); 
+    if((addr_lo>>>0).toString(16) == 'e000e004')
+        console.log(value_lo);
+    //console.log (`mem read 0x${(addr_lo>>>0).toString(16)}`); 
 }
 
 
 function write_fn (handle, type, addr_lo, addr_hi, size, value_lo, value_hi, user_data) {
-    console.log (`mem write 0x${addr_lo.toString(16)} -> 0x${value_lo.toString(16)}`); 
+    //console.log (`mem write 0x${(addr_lo>>>0).toString(16)} -> 0x${(value_lo>>>0).toString(16)}`); 
 }
 
 function read_unmapped_fn (handle, type, addr_lo, addr_hi, size, value_lo, value_hi, user_data) {
@@ -77,15 +79,15 @@ function interrupt_fn (handle, type, addr_lo, addr_hi, size, value_lo, value_hi,
     }
 }
 
-function read_register(handle, type, addr_lo, addr_hi, size, value_lo, value_hi, user_data) {
-    //console.log("Read");
-    console.log(`Read Register Address: ${(addr_lo>>>0).toString(16)}`);
-}
+// function read_register(handle, type, addr_lo, addr_hi, size, value_lo, value_hi, user_data) {
+//     //console.log("Read");
+//     console.log(`Read Register Address: ${(addr_lo>>>0).toString(16)}`);
+// }
 
-function write_register(handle, type, addr_lo, addr_hi, size, value_lo, value_hi, user_data) {
-    //console.log("Write");
-    console.log(`Write Register Address: ${(addr_lo>>>0).toString(16)}. ${(value_lo>>>0).toString(16)}`);
-}
+// function write_register(handle, type, addr_lo, addr_hi, size, value_lo, value_hi, user_data) {
+//     //console.log("Write");
+//     console.log(`Write Register Address: ${(addr_lo>>>0).toString(16)}. ${(value_lo>>>0).toString(16)}`);
+// }
 
 main();
 async function main() {
@@ -95,11 +97,6 @@ async function main() {
 
     console.log (firmware[0] + (firmware[1] << 8) + (firmware[2] << 16) + (firmware[3] << 24));
     console.log (firmware[4] + (firmware[5] << 8) + (firmware[6] << 16) + (firmware[7] << 24));
-
-    //Map registers and hook em
-    e.mem_map(REGISTER_START, REGISTER_END - REGISTER_START, uc.PROT_ALL);
-    e.hook_add(uc.HOOK_MEM_READ, read_register, 0, REGISTER_START, REGISTER_END - REGISTER_START, 0);
-    e.hook_add(uc.HOOK_MEM_WRITE, write_register, 0, REGISTER_START, REGISTER_END - REGISTER_START, 0);
     
     e.reg_write_i32(uc.ARM_REG_SP, firmware[0] + (firmware[1] << 8) + (firmware[2] << 16) + (firmware[3] << 24));
     e.reg_write_i32(uc.ARM_REG_PC, firmware[4] + (firmware[5] << 8) + (firmware[6] << 16) + (firmware[7] << 24));
@@ -108,13 +105,19 @@ async function main() {
     e.mem_map(FLASH_ADDRESS, FLASH_SIZE, uc.PROT_ALL);
     e.mem_map(RAM_ADDRESS, MAX_RAM_SIZE, uc.PROT_ALL);
 
-    e.mem_write(FLASH_ADDRESS, firmware);
-
-    // e.hook_add (uc.HOOK_MEM_WRITE, write_fn, 0, RAM_ADDRESS, RAM_ADDRESS+MAX_RAM_SIZE, 0);
-    // e.hook_add (uc.HOOK_MEM_READ, read_fn, 0, RAM_ADDRESS, RAM_ADDRESS+MAX_RAM_SIZE, 0);
+    //e.hook_add (uc.HOOK_MEM_WRITE, write_fn, 0, RAM_ADDRESS, RAM_ADDRESS+MAX_RAM_SIZE, 0);
+    //e.hook_add (uc.HOOK_MEM_READ, read_fn, 0, RAM_ADDRESS, RAM_ADDRESS+MAX_RAM_SIZE, 0);
     e.hook_add (uc.HOOK_MEM_READ_UNMAPPED, read_unmapped_fn);
     e.hook_add (uc.HOOK_MEM_WRITE_UNMAPPED, write_unmapped_fn);
     e.hook_add (uc.HOOK_INTR, interrupt_fn);
+
+    //Map registers and hook em
+    e.mem_map(REGISTER_START, REGISTER_END - REGISTER_START, uc.PROT_ALL);
+    e.hook_add(uc.HOOK_MEM_READ, read_fn, 0, REGISTER_START, REGISTER_END - REGISTER_START);
+    e.hook_add(uc.HOOK_MEM_WRITE, write_fn, 0, REGISTER_START, REGISTER_END - REGISTER_START);
+
+    e.mem_write(0xe000e004, Uint8Array.from([1]));
+    e.mem_write(FLASH_ADDRESS, firmware);
 
     function execution () {
 
